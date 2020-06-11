0cbcc3b7cc580b78fbb379d9d659bb79a23ca2cd
eden-kr <kzx789@naver.com>
2020-06-11 23:12:53 +0900

    initial commit

--- app/src/main/java/com/example/kpumap/MainActivity.kt
+++ app/src/main/java/com/example/kpumap/MainActivity.kt
@@ -5,13 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var searchText : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //검색어 가져오기
+        searchText = searchEdit.text.toString()
+
+        //검색 버튼 클릭 시
+        searchBtn.setOnClickListener {
+
+        }
+        //레스토랑 버튼 클릭 시
+        restaurant.setOnClickListener {
+
+        }
+        //술집 버튼 클릭 시
+        beer.setOnClickListener {
+
+        }
+        //카페 버튼 클릭 시
+        cafe.setOnClickListener {
+
+        }
     }
 }
